8ac46b325823d3e266655db83be2d1d457a02732
Abuzer ALACA <aalaca.33@gmail.com>
2020-01-28 11:15:39 +0100

    Updated HW.

--- homework/index.js
+++ homework/index.js
@@ -1,20 +1,20 @@
 'use strict';
 
 {
-  function fetchJSON(url, cb) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.responseType = 'json';
-    xhr.onload = () => {
-      if (xhr.status >= 200 && xhr.status <= 299) {
-        cb(null, xhr.response);
-      } else {
-        cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-      }
-    };
-    xhr.onerror = () => cb(new Error('Network request failed'));
-    xhr.send();
-  }
+  // function fetchJSON(url, cb) {
+  //   const xhr = new XMLHttpRequest();
+  //   xhr.open('GET', url);
+  //   xhr.responseType = 'json';
+  //   xhr.onload = () => {
+  //     if (xhr.status >= 200 && xhr.status <= 299) {
+  //       cb(null, xhr.response);
+  //     } else {
+  //       cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
+  //     }
+  //   };
+  //   xhr.onerror = () => cb(new Error('Network request failed'));
+  //   xhr.send();
+  // }
 
   function createAndAppend(name, parent, options = {}) {
     const elem = document.createElement(name);
@@ -29,23 +29,107 @@
     return elem;
   }
 
-  function renderRepoDetails(repo, ul) {
-    createAndAppend('li', ul, { text: repo.name });
+  function renderRepoDetails(repo, table) {
+    const titles = ['Repository:', 'Description:', 'Forks:', 'Updated:'];
+    const keys = ['name', 'description', 'forks_count', 'updated_at'];
+    for (let i = 0; i < titles.length; i++) {
+      const tr = createAndAppend('tr', table);
+      createAndAppend('th', tr, { text: titles[i] });
+      if (i === 0) {
+        const td = createAndAppend('td', tr);
+        createAndAppend('a', td, {
+          href: repo.html_url,
+          text: repo.name,
+          target: 'blank_',
+        });
+      } else {
+        createAndAppend('td', tr, { text: repo[keys[i]] });
+      }
+    }
   }
 
-  function main(url) {
-    fetchJSON(url, (err, repos) => {
-      const root = document.getElementById('root');
-      if (err) {
-        createAndAppend('div', root, {
+  function renderContributors(url, container) {
+    fetch(url)
+      .then(res => res.json())
+      .then(contributors => {
+        contributors.forEach(contributor => {
+          const divCont = createAndAppend('div', container, {
+            class: 'userContributors',
+          });
+          const img = createAndAppend('img', divCont);
+          img.src = contributor.avatar_url;
+
+          const pName = createAndAppend('p', divCont);
+
+          createAndAppend('a', pName, {
+            text: contributor.login,
+            href: contributor.html_url,
+            target:'_blank',
+          })
+          const pContributes = createAndAppend('p', divCont, {
+            class: 'contributes',
+          });
+
+          createAndAppend('span', pContributes, {
+            text:contributor.contributions
+          });
+        });
+      })
+      .catch(err => {
+        createAndAppend('div', container, {
           text: err.message,
           class: 'alert-error',
         });
-        return;
-      }
-      const ul = createAndAppend('ul', root);
-      repos.forEach(repo => renderRepoDetails(repo, ul));
+      });
+  }
+
+  function renderContents(repo, table, contributorsContainer) {
+    table.innerText = '';
+    contributorsContainer.innerText = '';
+
+    createAndAppend('h2', contributorsContainer, {
+      text: 'Contributors',
     });
+
+    const divContributors = createAndAppend('div', contributorsContainer, {
+      id: 'contTable',
+    });
+
+    renderRepoDetails(repo, table);
+    renderContributors(repo.contributors_url, divContributors);
+  }
+
+  function main(url) {
+    const root = document.getElementById('root');
+    const reposContainer = document.getElementById('repos');
+    const contributorsContainer = document.getElementById('contributors');
+    const select = document.querySelector('#repos-select');
+    const table = createAndAppend('table', reposContainer);
+
+    fetch(url)
+      .then(res => res.json())
+      .then(repos => {
+        repos
+          .sort((a, b) => {
+            return a.name.localeCompare(b.name);
+          })
+          .forEach((repo, index) => {
+            createAndAppend('option', select, {
+              value: index,
+              text: repo.name,
+            });
+          });
+
+        select.addEventListener('change', () => {
+          renderContents(repos[select.value], table, contributorsContainer);
+        });
+      })
+      .catch(err => {
+        createAndAppend('p', root, {
+          text: err.message,
+          class: 'alert-error',
+        });
+      });
   }
 
   const HYF_REPOS_URL =
